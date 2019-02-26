4d0625da01e038698e5642c78c5dd2e7136a7ca2
Jacob Hausmann <jacob@wireless-10-147-204-185.public.utexas.edu>
2019-02-25 19:16:51 -0600

    Add mapview address selection

---
+++ RepresentsMe/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  RepresentsMe
+//
+//  Created by Jacob Hausmann on 2/22/19.
+//  Copyright © 2019 11foot8. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
