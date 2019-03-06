7a7ad9398a6902a37e62517e967d139fe4b98174
bzsinger <benjamin.z.singer@gmail.com>
2019-03-06 17:58:47 -0600

    Set Officials' default cell profile image

--- RepresentsMe/Views/OfficialCell.swift
+++ RepresentsMe/Views/OfficialCell.swift
@@ -21,7 +21,7 @@ class OfficialCell: UITableViewCell {
             officeLabel.text = official?.office
             partyLabel.text = official?.party
 
-            portraitImageView.image = UIImage()
+            portraitImageView.image = UIImage(named: "default-photo.png")
             official?.getPhoto(completion: { (photoOfficial, image) in
                 // Ensure that photo is matched to correct cell
                 if (photoOfficial == self.official) {
