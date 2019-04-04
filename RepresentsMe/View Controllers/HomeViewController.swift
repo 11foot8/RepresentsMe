d0342fb2119a58b1536e33a8d94b2654d8eef5fa
bzsinger <benjamin.z.singer@gmail.com>
2019-04-03 21:00:30 -0500

    Modify HomeVC and MapVC to use shared LocationManager
    Move FontAwesome to Sources

--- RepresentsMe/View Controllers/HomeViewController.swift
+++ RepresentsMe/View Controllers/HomeViewController.swift
@@ -22,7 +22,7 @@ var userAddr = Address(streetAddress: "110 Inner Campus Drive",
 }
 var userAddrChanged = false
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // MARK: - Properties
     var addr: Address = userAddr
@@ -30,8 +30,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     // MARK: - Outlets
     @IBOutlet weak var officialsTableView: UITableView!
-
-    let locationManager = CLLocationManager()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -40,8 +38,17 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         officialsTableView.delegate = self
         officialsTableView.dataSource = self
 
-        checkLocationServices()
-        getOfficials(for: addr)
+        if (LocationManager.shared.checkLocationServices()) {
+            if let userLocation = LocationManager.shared.userLocation {
+                GeocoderWrapper.reverseGeocodeCoordinates(userLocation.coordinate) { (address: Address) in
+                    userAddr = address
+                    self.addr = userAddr
+                    self.getOfficials(for: userAddr)
+                }
+            }
+        } else {
+            getOfficials(for: addr)
+        }
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -68,82 +75,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
                     self.officialsTableView.reloadData()
                 }
             }
-        }
-    }
-
-    /// Check that location services are enabled, if so set up services,
-    /// if not alert user that location services are not enabled.
-    func checkLocationServices() {
-        // Check if Location Services are enabled globally
-        if CLLocationManager.locationServicesEnabled() {
-            setupLocationManager()
-            checkLocationAuthorization()
-        } else {
-            // TODO: show alert for letting user know they have to turn this on
-        }
-    }
-
-    /// Do setup for locationManager.
-    func setupLocationManager() {
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    }
-
-    /// Check what location authorization the application has, and alert user if
-    /// they need to take action to enable location authorization.
-    func checkLocationAuthorization() {
-        switch CLLocationManager.authorizationStatus() {
-        case .authorizedWhenInUse, .authorizedAlways:
-            locationManager.startUpdatingLocation()
-//            getReverseGeocode()
-            break
-        case .denied:
-            // TODO: show alert instructing them how to turn on permissions
-            break
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-            break
-        case .restricted:
-            // TODO: show an alert letting them know whats up
-            break
-        }
-    }
-
-    /// Gets user location for finding Officials
-    func getReverseGeocode() {
-        let location = locationManager.location    // Current coordinates to geocode
-        let geoCoder = CLGeocoder()              // Geocoder instance to use
-
-        if location == nil {
-            return
-        }
-
-        // Request will come back with 'placemarks' and 'error' as parameters
-        geoCoder.reverseGeocodeLocation(location!) { (placemarks, error) in
-            // If an error occured, alert user and return immediately
-            if let _ = error {
-                // TODO: Show alert informing the user
-                return
-            }
-
-            // placemark is a list of results, if no results returned, alert user and return immediately
-            guard let placemark = placemarks?.first else {
-                // TODO: Show alert informing the user
-                return
-            }
-
-            // Get address from the placemark, save it, and reload Officials
-            userAddr = Address(with: placemark)
-            self.addr = userAddr
-            self.getOfficials(for: userAddr)
-        }
-    }
-
-    // MARK: CLLocationManagerDelegate
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if (status == .authorizedAlways || status == .authorizedWhenInUse) {
-            locationManager.startUpdatingLocation()
-//            getReverseGeocode()
         }
     }
 
