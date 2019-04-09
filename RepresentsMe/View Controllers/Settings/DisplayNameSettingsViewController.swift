692aa09ee7922e950232f991843aa056923789c2
Jacob Hausmann <jacob@wireless-10-147-220-30.public.utexas.edu>
2019-04-09 13:36:31 -0500

    Implemented change displayname settings change

--- RepresentsMe/View Controllers/Settings/DisplayNameSettingsViewController.swift
+++ RepresentsMe/View Controllers/Settings/DisplayNameSettingsViewController.swift
@@ -9,13 +9,48 @@
 import UIKit
 
 class DisplayNameSettingsViewController: UIViewController {
+    // MARK: - Properties
+    let usersDB = UsersDatabase.getInstance()
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        displayNameTextField.text = usersDB.getCurrentUserDisplayName()
         // Do any additional setup after loading the view.
     }
-    
+
+    // MARK: - Outlets
+    @IBOutlet weak var displayNameTextField: UITextField!
+
+    // MARK: - Actions
+    // TODO: Disable save button until all fields are valid
+    @IBAction func saveTouchUp(_ sender: Any) {
+        self.view.endEditing(true)
+        // TODO: Start loading animation
+        guard let displayName = displayNameTextField.text else { return }
+
+        usersDB.changeUserDisplayName(newDisplayName: displayName) { (error) in
+            if let _ = error {
+                // TODO: Handle error
+                // TODO: Stop loading animation
+                let alert = UIAlertController(
+                    title: "Error",
+                    message: "\(error.debugDescription)",
+                    preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                // TODO: Stop loading animation
+                let alert = UIAlertController(
+                    title: "Saved",
+                    message: nil,
+                    preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
