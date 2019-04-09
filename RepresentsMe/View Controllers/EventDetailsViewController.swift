349ddbff3ef38e4aab2d74c8acf5b753bf51fb7d
varunadiga <varun.adiga@utexas.edu>
2019-04-09 11:38:10 -0500

    created all the new stuff, still need to get functionality working

---
+++ RepresentsMe/View Controllers/EventDetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EventDetailsViewController.swift
+//  RepresentsMe
+//
+//  Created by Varun Adiga on 4/9/19.
+//  Copyright © 2019 11foot8. All rights reserved.
+//
+
+import Foundation
---
+++ RepresentsMe/View Controllers/EventListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EventListViewController.swift
+//  RepresentsMe
+//
+//  Created by Varun Adiga on 4/2/19.
+//  Copyright © 2019 11foot8. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let EVENT_CELL_IDENTIFIER = "eventCell"
+
+class EventListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var eventTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        eventTableView.delegate = self
+        eventTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
