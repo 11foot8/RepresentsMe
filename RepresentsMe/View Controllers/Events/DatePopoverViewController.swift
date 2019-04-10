764dc6ed5b9829f36e94b937d893c4e7135f381d
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2019-04-10 13:23:58 -0500

    Add comments to date popover view controller

--- RepresentsMe/View Controllers/Events/DatePopoverViewController.swift
+++ RepresentsMe/View Controllers/Events/DatePopoverViewController.swift
@@ -8,24 +8,25 @@
 
 import UIKit
 
+/// The protocol to implement to receive the date when a date is selected
 protocol DatePopoverViewControllerDelegate {
     func didSelectDate(date: Date)
 }
 
+/// View controller to show a popover to select a date
 class DatePopoverViewController: UIViewController {
 
     @IBOutlet weak var datePicker: UIDatePicker!
+    
     var delegate: DatePopoverViewControllerDelegate?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
+    /// Send the selected date to the delegate and close
     @IBAction func saveTapped(_ sender: Any) {
         delegate?.didSelectDate(date: datePicker.date)
         dismiss(animated: false, completion: nil)
     }
 
+    /// Close without sending the date to the delegate
     @IBAction func cancelTapped(_ sender: Any) {
         dismiss(animated: false, completion: nil)
     }
