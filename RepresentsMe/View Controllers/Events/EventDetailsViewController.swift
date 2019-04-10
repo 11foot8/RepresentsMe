6631436539ccbbc8b166bc61630f872d6b7a0a2a
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2019-04-10 13:18:52 -0500

    Refactor viewWillAppear for event details

--- RepresentsMe/View Controllers/Events/EventDetailsViewController.swift
+++ RepresentsMe/View Controllers/Events/EventDetailsViewController.swift
@@ -13,6 +13,7 @@ import MapKit
 let EDIT_EVENT_SEGUE = "editEventSegue"
 
 class EventDetailsViewController: UIViewController {
+    
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var portraitImageView: UIImageView!
     @IBOutlet weak var eventNameLabel: UILabel!
@@ -21,44 +22,23 @@ class EventDetailsViewController: UIViewController {
     @IBOutlet weak var editButton: UIBarButtonItem!
     @IBOutlet weak var deleteEventButton: UIButton!
 
-    var event:Event?
-    var delegate:EventListDelegate?
+    var event:Event?                    // The Event to display
+    var delegate:EventListDelegate?     // The delegate to update
 
+    /// Sets up the view for the Event to display
     override func viewWillAppear(_ animated: Bool) {
-        if event == nil {
-            return
-        }
+        if let event = self.event {
+            // Set the labels
+            self.setLabels()
 
-        if let eventPhoto = event!.official?.photo {
-            portraitImageView.image = eventPhoto
-        }
+            // Set the photo
+            self.setPhoto()
 
-        eventNameLabel.text = event!.name
+            // Center the map on the location for the event
+            self.setupMapView()
 
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMMM d, h:mm a"
-        eventDateLabel.text = dateFormatter.string(from: event!.date)
-
-        self.eventLocationLabel.text = ""
-        GeocoderWrapper.reverseGeocodeCoordinates(event!.location) { (address: Address) in
-            self.eventLocationLabel.text = address.description
-        }
-
-        let pLat = event!.location.latitude
-        let pLong = event!.location.longitude
-        let center = CLLocationCoordinate2D(latitude: pLat, longitude: pLong)
-
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
-
-        self.mapView.setRegion(region, animated: true)
-
-        editButton.isEnabled = false
-        deleteEventButton.isHidden = true
-        deleteEventButton.isEnabled = false
-        if (UsersDatabase.shared.getCurrentUserUID() == event?.owner) {
-            editButton.isEnabled = true
-            deleteEventButton.isEnabled = true
-            deleteEventButton.isHidden = false
+            // Set whether or not the user can edit the event
+            self.setEditable()
         }
     }
 
@@ -82,6 +62,54 @@ class EventDetailsViewController: UIViewController {
             let destination = segue.destination as! CreateEventViewController
             destination.event = event
             destination.delegate = delegate
+        }
+    }
+    
+    /// Sets the labels for the Event
+    private func setLabels() {
+        eventNameLabel.text = event.name
+        eventDateLabel.text = event.formattedDate
+        
+        // Set the location
+        self.eventLocationLabel.text = ""
+        GeocoderWrapper.reverseGeocodeCoordinates(event.location) {(address) in
+            self.eventLocationLabel.text = address.description
+        }
+    }
+    
+    /// Sets the photo for the Event
+    private func setPhoto() {
+        if let event = self.event, let photo = event.official?.photo {
+            portraitImageView.image = photo
+        }
+    }
+    
+    /// Centers the map on the location for the Event
+    private func setupMapView() {
+        if let event = self.event {
+            let span = MKCoordinateSpan(latitudeDelta: 0.1,
+                                        longitudeDelta: 0.1)
+            let region = MKCoordinateRegion(center: event.location, span: span)
+            self.mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    /// Sets up the edit views based on whether or not the user is allowed to
+    /// edit the Event
+    private func setEditable() {
+        if let event = self.event {
+            if (UsersDatabase.shared.getCurrentUserUID() == event.owner) {
+                // User owns the event, let them edit it
+                editButton.isEnabled = true
+                editButton.title = "Edit"
+                deleteEventButton.isEnabled = true
+                deleteEventButton.isHidden = false
+            } else {
+                // User does not own the event, do not let them edit it
+                editButton.isEnabled = false
+                deleteEventButton.isHidden = true
+                deleteEventButton.isEnabled = false
+            }
         }
     }
 }
