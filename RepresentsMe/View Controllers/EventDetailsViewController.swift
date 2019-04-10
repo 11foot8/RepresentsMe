b40c553f3f65cf6eacea886f4309c25bd4baeb67
bzsinger <benjamin.z.singer@gmail.com>
2019-04-09 21:05:42 -0500

    Add EventDetailsVC

--- RepresentsMe/View Controllers/EventDetailsViewController.swift
+++ RepresentsMe/View Controllers/EventDetailsViewController.swift
@@ -19,5 +19,32 @@ class EventDetailsViewController: UIViewController {
 
     var event:Event?
 
+    override func viewWillAppear(_ animated: Bool) {
+        if event == nil {
+            return
+        }
 
+        if let eventPhoto = event!.official?.photo {
+            portraitImageView.image = eventPhoto
+        }
+
+        eventNameLabel.text = event!.name
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, h:mm a"
+        eventDateLabel.text = dateFormatter.string(from: event!.date)
+
+        self.eventLocationLabel.text = ""
+        GeocoderWrapper.reverseGeocodeCoordinates(event!.location) { (address: Address) in
+            self.eventLocationLabel.text = address.description
+        }
+
+        let pLat = event!.location.latitude
+        let pLong = event!.location.longitude
+        let center = CLLocationCoordinate2D(latitude: pLat, longitude: pLong)
+
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+
+        self.mapView.setRegion(region, animated: true)
+    }
 }
