d968deefcb337549309fc9a8e58b2c2b066df4d8
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2019-04-11 11:21:47 -0500

    Refactor display name settings view controller

--- RepresentsMe/ViewControllers/Settings/DisplayNameSettingsViewController.swift
+++ RepresentsMe/ViewControllers/Settings/DisplayNameSettingsViewController.swift
@@ -8,20 +8,25 @@
 
 import UIKit
 
+/// The view controller for the user to change their display name
 class DisplayNameSettingsViewController: UIViewController {
-    // MARK: - Properties
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var displayNameTextField: UITextField!
 
     // MARK: - Lifecycle
+    
+    /// Set the user's current display name
     override func viewDidLoad() {
         super.viewDidLoad()
         displayNameTextField.text = UsersDatabase.currentUserDisplayName
     }
 
-    // MARK: - Outlets
-    @IBOutlet weak var displayNameTextField: UITextField!
-
     // MARK: - Actions
     // TODO: Disable save button until all fields are valid
+    
+    /// Update the user's display name when the save button is pressed
     @IBAction func saveTouchUp(_ sender: Any) {
         self.view.endEditing(true)
         // Start loading animation
@@ -29,41 +34,18 @@ class DisplayNameSettingsViewController: UIViewController {
         let hud = LoadingHUD(self.view)
         guard let displayName = displayNameTextField.text else { return }
 
-        UsersDatabase.shared.changeUserDisplayName(newDisplayName: displayName) { (error) in
-            if let _ = error {
-                // TODO: Handle error
-                // End loading animation
-                hud.end()
-                self.navigationItem.hidesBackButton = false
-                let alert = UIAlertController(
-                    title: "Error",
-                    message: "\(error.debugDescription)",
-                    preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default))
-                self.present(alert, animated: true, completion: nil)
+        UsersDatabase.shared.changeUserDisplayName(
+            newDisplayName: displayName) {(error) in
+            
+            // Stop the loading animation
+            hud.end()
+            self.navigationItem.hidesBackButton = false
+            
+            if let error = error {
+                self.alert(title: "Error", message: error.localizedDescription)
             } else {
-                // End loading animation
-                hud.end()
-                self.navigationItem.hidesBackButton = false
-                let alert = UIAlertController(
-                    title: "Saved",
-                    message: nil,
-                    preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default))
-
-                self.present(alert, animated: true, completion: nil)
+                self.alert(title: "Saved")
             }
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
